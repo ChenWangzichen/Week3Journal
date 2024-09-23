5ebc3acfd9e7bd109a8d0d5d97a542e927e7524f
ChenWangzichen <148375673+ChenWangzichen@users.noreply.github.com>
2024-09-23 10:58:54 -0400

    task 1A done

--- Zichen-WAng-Week3Journal/Assets/Scripts/Controllers/Player.cs
+++ Zichen-WAng-Week3Journal/Assets/Scripts/Controllers/Player.cs
@@ -9,9 +9,36 @@ public class Player : MonoBehaviour
     public GameObject bombPrefab;
     public Transform bombsTransform;
 
+    //set 2 velocity for horizontal and vertical
+    private Vector3 vVelocity = new Vector3(1f,0);
+    private Vector3 hVelocity = new Vector3(0,1f);
+
     void Update()
     {
-
+        //call player movement method
+        PlayerMovement();
     }
-
+    public void PlayerMovement()
+    {
+        //press leftArrow, x-1
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position -= vVelocity*Time.deltaTime;
+        }
+        //press right, x+1
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position += vVelocity*Time.deltaTime;
+        }
+        //press up, y+1
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.position += hVelocity*Time.deltaTime;
+        }
+        //press down, y-1
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.position -= hVelocity*Time.deltaTime;
+        }
+    }
 }
